cab32478c5497400fbc42c487957797d9eb7ff3f
Gia Bao <baotg0502@gmail.com>
2025-04-24 10:58:13 +0700

    Rerun pipeline (Create S3, DynamoDB for dev) #2

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,8 +1,3 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = module.vpc.vpc_id
-}
-
 /*output "eks_cluster_name" {
   description = "Name of the EKS cluster"
   value       = module.eks.cluster_name
