f40a9902fe526245aac09bc9a721f4b21f8d9d37
Trần Gia Bảo <142662904+Giabaoday@users.noreply.github.com>
2025-04-30 18:48:36 +0700

    Update outputs.tf

--- terraform/common/outputs.tf
+++ terraform/common/outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   description = "ID of the VPC"
   value       = module.vpc.vpc_id
 }
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = module.vpc.private_subnets
+}
