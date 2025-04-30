c221b57ed5c3a6d683cd81df7128f7ce8af6cedd
Gia Bao <baotg0502@gmail.com>
2025-04-30 18:57:20 +0700

    Create EKS #2

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -149,7 +149,7 @@ module "eks" {
   source = "../../modules/eks"
 
   vpc_id                 = data.terraform_remote_state.common.outputs.vpc_id
-  vpc_private_subnet_ids = data.terraform_remote_state.common.outputs.vpc_private_subnet_ids
+  vpc_private_subnet_ids = data.terraform_remote_state.common.outputs.private_subnet_ids
 
   tags = {
     Environment = var.environment
