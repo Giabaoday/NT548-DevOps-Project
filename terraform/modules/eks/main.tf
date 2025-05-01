8571b1c91d4ec672a66090189bdc70e45e279522
Gia Bao <baotg0502@gmail.com>
2025-05-01 16:49:26 +0700

    Update EKS

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -24,8 +24,8 @@ module "eks" {
       instance_types = ["t3.micro"]
 
       min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      max_size     = 3
+      desired_size = 2
     }
 
     argocdnodegroup = {
@@ -33,8 +33,8 @@ module "eks" {
       instance_types = ["t3.micro"]
 
       min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      max_size     = 3
+      desired_size = 2
     }
   }
   tags = var.tags
