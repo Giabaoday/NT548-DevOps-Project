2883c69988493b5af6d6e79a5f4eeba35703348f
Trần Gia Bảo <142662904+Giabaoday@users.noreply.github.com>
2025-05-29 11:05:28 +0700

    Sync common main.tf

--- terraform/common/main.tf
+++ terraform/common/main.tf
@@ -28,3 +28,19 @@ module "vpc" {
     ManagedBy   = "Terraform"
   }
 }
+
+#Tạo EKS cluster
+module "eks" {
+  source = "../modules/eks"
+
+  cluster_name           = "${var.project_name}-eks"
+  vpc_id                 = module.vpc.vpc_id
+  vpc_private_subnet_ids = module.vpc.private_subnets
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+    ManagedBy   = "Terraform"
+    Application = "Product Traceability"
+  }
+}
